882aa12cb68793341b2581f82dc4bbabac51964f
koyamot <koyamot@gmail.com>
2022-08-25 21:47:03 -0700

    apollo installed

--- src/index.js
+++ src/index.js
@@ -3,9 +3,40 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000/graphql",
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query GetUsers {
+        getUsers {
+          email
+          profile {
+            firstName
+            lastName
+            bio
+          }
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
