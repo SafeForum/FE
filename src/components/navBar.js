2c83aa98b012901b2abb42963ae7283881eead0b
koyamot <koyamot@gmail.com>
2023-02-01 12:44:45 -0800

    added nav bar

---
+++ src/components/navBar.js
@@ -0,0 +1,13 @@
+// import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <>
+        <h1 className="text-3xl font-bold">
+      NavBar
+    </h1>  
+    </>
+  );
+}
+
+export default Navbar;
