920e345fb8d415b99c2791a70575502607047cb7
koyamot <koyamot@gmail.com>
2023-02-01 13:31:49 -0800

    Add log register routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { Routes, Route } from "react-router-dom"
-import Homepage from './pages/homepage'
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/homepage";
 import Navbar from "./components/navBar";
 
 export default function App() {
   return (
     <>
-    <Navbar />
-    <Routes>
-    <Route path="/" element={<Homepage />}/>
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Homepage />} />
+        <Route path="/register" element={<Homepage />} />
+      </Routes>
     </>
-    );
+  );
 }
