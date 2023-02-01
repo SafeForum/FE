a0d3772e00198c0894623ccc3a6e967a6e85ad61
koyamot <koyamot@gmail.com>
2023-02-01 12:47:47 -0800

    AuthProvider isnt working as it should/disabled

--- src/index.js
+++ src/index.js
@@ -2,42 +2,38 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from 'react-router-dom';
-import client from "./apolloClient"
+import { BrowserRouter } from "react-router-dom";
+import client from "./apolloClient";
 import { AuthProvider } from "./context/authContext";
-import {
-  ApolloProvider,
-  gql,
-} from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
 
 //React App needs access to:
 //Client
 // Auth Context
 // Browser Router to /login /register
 
-
-client
-  .query({
-    query: gql`
-      query GetUsers {
-        getUsers{
-          email
-          profile {
-            occupation
-          }
-        }
-      }
-    `,
-  })
-  .then((result) => console.log(result));
+// client
+//   .query({
+//     query: gql`
+//       query GetUsers {
+//         getUsers{
+//           email
+//           profile {
+//             occupation
+//           }
+//         }
+//       }
+//     `,
+//   })
+//   .then((result) => console.log(result));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <AuthProvider>
+  <BrowserRouter>
+    {/* <AuthProvider> */}
     <ApolloProvider client={client}>
-    <BrowserRouter>
       <App />
-  </BrowserRouter>
     </ApolloProvider>
-    </AuthProvider>,
+    {/* </AuthProvider> */}
+  </BrowserRouter>
 );
