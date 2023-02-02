7036c27aefb0bb74c433703276fef7fa83248eb8
koyamot <koyamot@gmail.com>
2023-02-02 14:07:29 -0800

    remove register component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
 import Navbar from "./components/navBar";
-import Login from "./pages/login";
-import Register from "./pages/register";
+import Login from "./pages/login"
 
 export default function App() {
   return (
@@ -11,7 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/register" element={<Homepage />} />
         <Route path="/dashboard" element={<Homepage />} />
       </Routes>
     </>
