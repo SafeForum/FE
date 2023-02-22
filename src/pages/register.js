e1a6ee8f774106ec2693cc2a10281c0b1983e409
George Hatzigeorgio <georgehatzigeorgio@zohomail.com>
2023-02-22 09:43:46 -0500

    changed date-of-birth placeholder

--- src/pages/register.js
+++ src/pages/register.js
@@ -161,7 +161,7 @@ return <Loading/>  }
           <input
             name="dob"
             type="date"
-            placeholder="dob"
+            placeholder="date of birth"
             id="dob"
             className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
             onChange={onChange}
