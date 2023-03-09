90ddd682dc2fe7124d67f8e55d7efaa32db4b49d
koyamot <koyamot@gmail.com>
2023-03-08 20:35:33 -0800

    adding Register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,11 +1,12 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { AuthContext } from "../context/authContext";
 import { useForm } from "../utilities/hooks";
 import { gql, useMutation } from "@apollo/client";
 import { Link, useNavigate } from "react-router-dom";
+import Loading from "../components/Loading";
 
 const CREATE_USER = gql`
-  mutation CreateUser($userInput: UserInput, $profileInput: ProfileInput) {
+  mutation CreateUser($userInput: UserInput!, $profileInput: ProfileInput) {
     createUser(userInput: $userInput, profileInput: $profileInput) {
       token
       userId
@@ -16,6 +17,13 @@ const CREATE_USER = gql`
 
 const Register = (props) => {
   let navigate = useNavigate();
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(false);
+    setTimeout(() => {}, 8000);
+  }, []);
 
   //dont need context yet
   const context = useContext(AuthContext);
@@ -60,7 +68,7 @@ const Register = (props) => {
   const [login, { loading }] = useMutation(CREATE_USER, {
     update(proxy, { data: { createUser: userData } }) {
       context.login(userData);
-      navigate("/");
+      navigate("/dashboard");
     },
     onError({ graphQLErrors }) {
       setErrors(graphQLErrors);
@@ -73,170 +81,142 @@ const Register = (props) => {
   });
 
   if (loading) {
-    <h1>error loading</h1>;
+    return <Loading />;
   }
 
   return (
     <>
+      <div className=" mb-40 text-center font-semibold font-serif mt-4">
+        REGISTER
+      </div>
       <form onSubmit={onSubmit}>
-        <div className="max-w-md px-4 mx-auto mt-12">
+        <div className="max-w-md px-4 mx-auto">
           <label htmlFor="email" className="block py-2 text-gray-500">
             Email
           </label>
-          <div className="flex items-center text-gray-400 border rounded-md">
-            <div className="px-3 py-2.5 rounded-l-md bg-gray-50 border-r"></div>
-            <input
-              name="email"
-              type="text"
-              placeholder="email"
-              id="email"
-              className="w-full p-2.5 ml-2 bg-transparent outline-none"
-              required
-              onChange={onChange}
-            />
-          </div>
+          <input
+            name="email"
+            type="text"
+            placeholder="email"
+            id="email"
+            className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+            required
+            onChange={onChange}
+          />
         </div>
-        <div className="max-w-md px-4 mx-auto mt-12">
+        <div className="max-w-md px-4 mx-auto ">
           <label htmlFor="password" className="block py-2 text-gray-500">
             Password
           </label>
-          <div className="flex items-center text-gray-400 border rounded-md">
-            <div className="px-3 py-2.5 rounded-l-md bg-gray-50 border-r"></div>
-            <input
-              name="password"
-              type="text"
-              placeholder="passwor"
-              id="password"
-              className="w-full p-2.5 ml-2 bg-transparent outline-none"
-              onChange={onChange}
-              required
-            />
-          </div>
+          <input
+            name="password"
+            type="password"
+            placeholder="password"
+            id="password"
+            className="w-full mt-2 px-3 py-2  bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+            onChange={onChange}
+            required
+          />
         </div>
-        <div className="max-w-md px-4 mx-auto mt-12">
-          <label htmlFor="firstName" className="block py-2 text-gray-500">
+        <div className="max-w-md px-4 mx-auto">
+          <label htmlFor="first name" className="block py-2 text-gray-500">
             First Name
           </label>
-          <div className="flex items-center text-gray-400 border rounded-md">
-            <div className="px-3 py-2.5 rounded-l-md bg-gray-50 border-r"></div>
-            <input
-              name="firstName"
-              type="text"
-              placeholder="firstName"
-              id="firstName"
-              className="w-full p-2.5 ml-2 bg-transparent outline-none"
-              onChange={onChange}
-              required
-            />
-          </div>
+          <input
+            name="first name"
+            type="text"
+            placeholder="first name"
+            id="firstName"
+            className="w-full  px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+            onChange={onChange}
+            required
+          />
         </div>
-        <div className="max-w-md px-4 mx-auto mt-12">
-          <label htmlFor="lastName" className="block py-2 text-gray-500">
+        <div className="max-w-md px-4 mx-auto">
+          <label htmlFor="last name" className="block py-2 text-gray-500">
             lastName
           </label>
-          <div className="flex items-center text-gray-400 border rounded-md">
-            <div className="px-3 py-2.5 rounded-l-md bg-gray-50 border-r"></div>
-            <input
-              type="text"
-              name="lastName"
-              placeholder="lastName"
-              id="lastName"
-              className="w-full p-2.5 ml-2 bg-transparent outline-none"
-              onChange={onChange}
-              required
-            />
-          </div>
+
+          <input
+            type="text"
+            name="last name"
+            placeholder="last name"
+            id="last name"
+            className="w-full  px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+            onChange={onChange}
+            required
+          />
         </div>
-        <div className="max-w-md px-4 mx-auto mt-12">
+
+        <div className="max-w-md px-4 mx-auto">
           <label htmlFor="Date of Birth" className="block py-2 text-gray-500">
             Date of Birth
           </label>
-          <div className="flex items-center text-gray-400 border rounded-md">
-            <div className="px-3 py-2.5 rounded-l-md bg-gray-50 border-r"></div>
-            <input
-              name="dob"
-              type="date"
-              placeholder="dob"
-              id="dob"
-              className="w-full p-2.5 ml-2 bg-transparent outline-none"
-              onChange={onChange}
-              required
-            />
-          </div>
+
+          <input
+            name="dob"
+            type="date"
+            placeholder="date of birth"
+            id="date of birth"
+            className="w-full px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+            onChange={onChange}
+            required
+          />
         </div>
-        <div className="max-w-md px-4 mx-auto mt-12">
+        <div className="max-w-md px-4 mx-auto">
           <label htmlFor="city" className="block py-2 text-gray-500">
             City
           </label>
-          <div className="flex items-center text-gray-400 border rounded-md">
-            <div className="px-3 py-2.5 rounded-l-md bg-gray-50 border-r"></div>
-            <input
-              name="city"
-              type="text"
-              placeholder="city"
-              id="city"
-              className="w-full p-2.5 ml-2 bg-transparent outline-none"
-              onChange={onChange}
-              required
-            />
-          </div>
+
+         <label htmlFor="state" className="block py-2 text-gray-"></label>
+          <input
+            name="state"
+            type="text"
+            placeholder="state"
+            id="state"
+            className="w-full  px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+            onChange={onChange}
+            required
+          />
         </div>
-        <div className="max-w-md px-4 mx-auto mt-12">
-          <label htmlFor="state" className="block py-2 text-gray-500">
-            State
-          </label>
-          <div className="flex items-center text-gray-400 border rounded-md">
-            <div className="px-3 py-2.5 rounded-l-md bg-gray-50 border-r"></div>
-            <input
-              name="state"
-              type="text"
-              placeholder="state"
-              id="state"
-              className="w-full p-2.5 ml-2 bg-transparent outline-none"
-              onChange={onChange}
-              required
-            />
-          </div>
-        </div>
-        <div className="max-w-md px-4 mx-auto mt-12">
+
+        <div className="max-w-md px-4 mx-auto ">
           <label htmlFor="occupation" className="block py-2 text-gray-500">
             Occupation
           </label>
-          <div className="flex items-center text-gray-400 border rounded-md">
-            <div className="px-3 py-2.5 rounded-l-md bg-gray-50 border-r"></div>
-            <input
-              name="occupation"
-              type="text"
-              placeholder="occupation"
-              id="occupation"
-              className="w-full p-2.5 ml-2 bg-transparent outline-none"
-              onChange={onChange}
-              required
-            />
-          </div>
+
+          <input
+            name="occupation"
+            type="text"
+            placeholder="occupation"
+            id="occupation"
+            className="w-full  px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+            onChange={onChange}
+            required
+          />
         </div>
-        <div className="max-w-md px-4 mx-auto mt-12">
-          <label htmlFor="state" className="block py-2 text-gray-500">
+        <div className="max-w-md px-4 mx-auto">
+          <label htmlFor="bio" className="block py-2 text-gray-500">
             Bio
           </label>
-          <div className="flex items-center text-gray-400 border rounded-md">
-            <div className="px-3 py-2.5 rounded-l-md bg-gray-50 border-r"></div>
-            <textarea
-              name="bio"
-              type="text"
-              placeholder="bio"
-              id="state"
-              rows="4"
-              col="50"
-              className="w-full p-2.5 ml-2 bg-transparent outline-none"
-              onChange={onChange}
-              required
-            />
-          </div>
+          <textarea
+            textarea=""
+            name="bio"
+            type="textarea"
+            placeholder="bio"
+            id="bio"
+            rows="6"
+            col="50"
+            className="w-full mb-40 mt-2 px-3 py-2 text-gray-500
+          bg-transparent outline-none border focus:border-indigo-600 shadow-sm
+          rounded-lg"
+            onChange={onChange}
+            required
+          />
         </div>
         <div className="flex justify-center">
-          <button className="px-5 py-3 text-white duration-150 bg-indigo-600 rounded-lg hover:bg-indigo-700 active:shadow-lg">
-            Signup
+          <button className="px-7 py-4 text-white duration-150 bg-indigo-600 rounded-lg hover:bg-indigo-700 acrtive:shadow">
+            REGISTER
           </button>
         </div>
       </form>
