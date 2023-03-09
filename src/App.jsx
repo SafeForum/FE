a8f39f4238ff2f851636723c3fc1c01edef4fa7e
koyamot <koyamot@gmail.com>
2023-03-08 20:36:36 -0800

    change case in Register import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
 import Navbar from "./components/navBar";
 import Login from "./pages/login"
-import Register from "./pages/register";
+import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 export default function App() {
   return (
