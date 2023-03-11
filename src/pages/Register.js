cfb202df1f57533cb77871b7a3b8e129b0fa01a3
George Hatzigeorgio <georgehatzigeorgio@zohomail.com>
2023-03-10 19:51:32 -0500

    register page almost done

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -87,6 +87,12 @@ const Register = (props) => {
   return (
     <>
       <div className=" mb-40 text-center font-semibold font-serif mt-4">
+      <img
+            src="https://floatui.com/logo.svg"
+            width={150}
+            className="mx-auto"
+            alt="logo"
+          />
         REGISTER
       </div>
       <form onSubmit={onSubmit}>
@@ -167,14 +173,24 @@ const Register = (props) => {
           <label htmlFor="city" className="block py-2 text-gray-500">
             City
           </label>
-
+          <input 
+          type='text'
+          name= 'city'
+          id= 'city'
+          placeholder= 'city'
+          classname="w-full px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg "
+          onChange={onChange}
+          required
+    />
+    </div>
+        <div className="max-w-md px-4 mx-auto">
          <label htmlFor="state" className="block py-2 text-gray-"></label>
           <input
             name="state"
             type="text"
             placeholder="state"
             id="state"
-            className="w-full  px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+            className="w-full  px-3 py-2w text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
             onChange={onChange}
             required
           />
