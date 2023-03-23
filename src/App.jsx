d1e903825349295859be7b164c188481dd8ce4ad
Gabriel Henton <59454894+gsauce7@users.noreply.github.com>
2023-03-22 18:45:45 -0700

    profile route added to app.js and profile.js start

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Homepage from "./pages/homepage";
 import Navbar from "./components/navBar";
 import Login from "./pages/login"
 import Register from "./pages/register";
+import Profile from "./pages/profile";
 
 export default function App() {
   return (
@@ -12,7 +13,7 @@ export default function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
+        <Route path="/profile" element={<Profile />} />
         <Route path="/dashboard" element={<Homepage />} />
       </Routes>
     </>
---
+++ src/pages/profile.js
@@ -0,0 +1,16 @@
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "../utilities/hooks";
+import { gql, useMutation } from "@apollo/client";
+import { AuthContext } from "../context/authContext";
+
+
+
+function Profile(props) {
+    <>
+    
+      
+    </>
+}
+
+export default Profile;
