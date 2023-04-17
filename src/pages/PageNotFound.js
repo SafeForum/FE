3772a71885fc70766001ec59e2ec929fe92fe980
Andrea Cuevas <andicuevasp@gmail.com>
2023-04-17 15:09:57 +0200

    added styles to 404 page

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 import notFoundIcon from "../assets/not-found-icon.svg"
 export default function PageNotFound() {
     return (
-        <div>
+        <div className="w-full flex flex-wrap items-center justify-center mt-60">
             <img src={notFoundIcon} alt="Page not found" />
-            <h1>Oops!</h1>
-            <h2>The page you're looking cannot be found!</h2>
-            <Link to="/dashboard/:cityPortal">Dashboard</Link>
+            <div className='flex-col flex m-12 w-1/5'>
+                <h1 className="text-5xl font-semibold">Oops!</h1>
+                <h1 className="text-2xl font-semibold mt-5 mb-5">Looks like the page you're looking for is nowhere to be found!</h1>
+                <Link to="/dashboard/:cityPortal" className="px-5 py-3 text-white duration-150 bg-indigo-600 rounded-lg hover:bg-indigo-700 active:shadow-lg">Back to Dashboard</Link>
+            </div>
         </div>
     )
 }
